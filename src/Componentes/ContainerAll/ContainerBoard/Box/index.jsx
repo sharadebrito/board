7bc4b83d098a7900a562c666aa078614a72645b9
Shara de Brito <sharadebrito@hotmail.com>
2025-08-21 20:13:17 -0300

    Implementação de button para exclusão e tooltip.

--- src/Componentes/ContainerAll/ContainerBoard/Box/index.jsx
+++ src/Componentes/ContainerAll/ContainerBoard/Box/index.jsx
@@ -1,10 +1,16 @@
 import styles from "./styles.module.css";
 
-export function Box({titulo, descrição}) {
+export function Box({ titulo, descrição }) {
+
   return (
     <div className={styles.box}>
-      <h1>{titulo}</h1>
+      <div className={styles.titulo}>
+        <h1>{titulo}</h1>
+        <button title="Excluir tarefa">X</button>
+      </div>
+
       <p>{descrição}</p>
+
       <div className={styles.buttons}>
         <button className={styles.toDo}>To Do</button>
         <button className={styles.doing}>Doing</button>
